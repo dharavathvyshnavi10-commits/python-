802ffc8fc5498a4868c41673224a73cb1b8c3984
dharavathvyshnavi10-commits <dharavathvyshnavi10@gmail.com>
2026-04-22 21:05:08 +0530

    Add if-else condition program for weird number check

---
+++ hackerrank2.py
@@ -0,0 +1,12 @@
+platform: hackerrank
+problem type: if-else condition
+program:
+n = int(input().strip())
+    if n%2!=0:
+        print("Weird")
+    elif 2<=n<=5:
+        print("Not Weird")
+    elif 6<=n<=20:
+        print("Weird")
+    else:
+        print("Not Weird")
